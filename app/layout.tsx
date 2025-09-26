6be8925f3599080a74970181536a423f188d918e
Krishna Gavali <krishnagavali973@gmail.com>
2025-09-27 00:39:29 +0530

    chore: initialize project with package.json, postcss.config.mjs, and tsconfig.json
    - Added package.json with dependencies and scripts for development, build, and linting.
    - Created postcss.config.mjs for Tailwind CSS integration.
    - Set up tsconfig.json with TypeScript compiler options and paths.

---
+++ app/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Poppins } from "next/font/google";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "Shadcn UI Tutorial",
+  description:
+    "A tutorial project using Shadcn UI with Next.js and Tailwind CSS",
+};
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${poppins.className} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage: React.FC = () => {
+  return (
+    <div className="">
+      <h1 className=" text-3xl">Hello, Next.js!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
